82017a7e235cf75182346e1708681a9aaaf7a445
suyeon-jung <wjdjsy210@gmail.com>
2021-07-14 21:51:08 +0900

    first commit

---
+++ 1_IO/1_11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << (b %10) * a << "\n";
+    cout << (b % 100 / 10) * a << "\n";
+    cout << (b / 100) * a << "\n";
+    cout <<  a * b << "\n";
+}
