bc35f770d72532f4ecd60b5f028a6a139e6bc84a
suyeon-jung <wjdjsy210@gmail.com>
2021-07-24 19:54:40 +0900

    [BOJ] Complete 7 step(9~10)

--- BOJ/7_string/1316.cpp
+++ BOJ/7_string/1316.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int wordCount = 0;
-  cin >> wordCount;
+  int word_count, group_count;
+  cin >> word_count;
+  group_count = word_count;
 
-  for (int i = 0; i < wordCount; i++) {
+  while (word_count > 0) {
     string input;
     cin >> input;
+
+    bool alphabet[26] = {false};
+    for (int i = 0; i < input.length(); i++) {
+      int idx = input[i] - 'a';
+      if (!alphabet[idx])
+        alphabet[idx] = true;
+      else if (alphabet[idx] && input[i - 1] != input[i]) {
+        group_count--;
+        break;
+      }
+    }
+    word_count--;
   }
+
+  cout << group_count << endl;
+
   return 0;
 }
--- BOJ/7_string/2941.cpp
+++ BOJ/7_string/2941.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+int main(int argc, char const *argv[]) {
+  int index;
+  string dict[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+  string input;
+  cin >> input;
+  for (int i = 0; i < 8; i++) {
+    // 해당 문자가 있다면 다른 문자로 치환
+    while (1) {
+      index = input.find(dict[i]);
+      if (index == string::npos) break;
+      input.replace(index, dict[i].length(), "#");
+    }
+  }
+  cout << input.length();
+  return 0;
 }
