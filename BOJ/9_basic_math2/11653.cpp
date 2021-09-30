1a270b57a12ec9e3665ce47e4d0bfd667716f3b6
suyeon-jung <wjdjsy210@gmail.com>
2021-09-30 23:39:27 +0900

    [BOJ] Complete 9 step(3, 4, 5)

--- BOJ/9_basic_math2/11653.cpp
+++ BOJ/9_basic_math2/11653.cpp
@@ -1,13 +1,39 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
 void solve();
+int getPrimeNum(int start, int n);
 int main(int argc, char const *argv[]) {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
 
   solve();
-
   return 0;
 }
-void solve() {}
+
+void solve() {
+  int N;
+  cin >> N;
+
+  int prime = 2;
+  // N이 1이 되는 순간 종료
+  while (N > 1) {
+    // 2부터 시작해서 더이상 나눠지지 않을때까지 같은 수로 계속 나눠
+    if (N % prime == 0) {  // 나눠지는 경우
+      cout << prime << "\n";
+    } else {  // 안 나눠지는 경우
+      prime = getPrimeNum(prime + 1, N);
+    }
+    N /= prime;
+  }
+}
+int getPrimeNum(int start, int n) {
+  for (int i = start; i <= n; i++) {
+    if (n % i == 0) {
+      cout << i << "\n";
+      return i;
+    }
+  }
+  return 0;
+}
--- BOJ/9_basic_math2/4948.cpp
+++ BOJ/9_basic_math2/4948.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void solve();
+int countPrimeNum(int n);
 bool isPrime(int n);
 int main(int argc, char const *argv[]) {
   ios::sync_with_stdio(false);
@@ -11,29 +12,37 @@ int main(int argc, char const *argv[]) {
   solve();
   return 0;
 }
-
 void solve() {
-  int M, N;
-  cin >> M >> N;
-
-  for (int i = M; i <= N; i++) {
-    if (isPrime(i) == true) {
-      cout << i << "\n";
+  int n;
+  while (true) {
+    cin >> n;
+    if (n == 0) {
+      break;
     }
+    cout << countPrimeNum(n) << "\n";
   }
 }
+int countPrimeNum(int n) {
+  int count = 0;
 
+  // n~2n 까지 소수인지 검사
+  for (int i = n + 1; i <= 2 * n; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
+}
 bool isPrime(int n) {
+  int limit = sqrt(n);
+
   if (n == 1) {
     return false;
   }
-
-  int limit = sqrt(n);
   for (int i = 2; i <= limit; i++) {
     if (n % i == 0) {
       return false;
     }
   }
-
   return true;
 }
