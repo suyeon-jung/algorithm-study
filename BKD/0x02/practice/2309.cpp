cfef5cf3bbaa7bb1e5b6dd463c2ce4f377fabd80
suyeon-jung <wjdjsy210@gmail.com>
2021-10-08 23:56:42 +0900

    [BKD] Modify lecture 2

--- BKD/0x02/practice/2309.cpp
+++ BKD/0x02/practice/2309.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int height[9];
-
+vector<int> height;
+int input, sum;
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   for (int i = 0; i < 9; i++) {
-    cin >> height[i];
+    cin >> input;
+    height.push_back(input);
+    sum += input;
   }
+  for (int i : height) cout << i << ' ';
+  cout << '\n';
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = i + 1; j < 9; j++) {
+      if (sum - (height[i] + height[j]) == 100) {
+        remove(height.begin(), height.end(), height[i]);
+        remove(height.begin(), height.end(), height[j]);
+        cout << i << ' ' << j;
+        break;
+      }
+    }
+  }
+  for (int i : height) cout << i << ' ';
 
   return 0;
 }
--- BKD/0x02/practice/2480.cpp
+++ BKD/0x02/practice/2480.cpp
@@ -2,33 +2,18 @@
 #include <iostream>
 using namespace std;
 
-int input, dice[7];
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  for (int i = 0; i < 3; i++) {
-    cin >> input;
-    dice[input]++;
-  }
+  int a[3];
+  cin >> a[0] >> a[1] >> a[2];
+  sort(a, a + 3);
 
-  int max_value = 0, max_index;
-  for (int i = 1; i < 7; i++) {
-    if (max_value < dice[i]) max_value = dice[i];
-    if (dice[i] >= 1) max_index = i;
-  }
-
-  //   int max = 0;
-  //   for (int i = 1; i < 7; i++) {
-  //     if (dice[i] == 3) {
-  //       cout << 10000 + i * 1000;
-  //       break;
-  //     } else if (dice[i] == 2) {
-  //       cout << 1000 + i * 100;
-  //       break;
-  //     } else
-  //       max = dice[i] == 1 ? i : max;
-  //   }
-
-  //   if (max != 0) cout << max * 100;
+  if (a[0] == a[2])
+    cout << 10000 + a[0] * 1000;
+  else if (a[0] == a[1] || a[1] == a[2])
+    cout << 1000 + a[1] * 100;
+  else
+    cout << a[2] * 100;
 }
