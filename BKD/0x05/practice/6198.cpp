8f4dc7c91332f44c7c4c4c4869c0b29268e5ee95
suyeon-jung <wjdjsy210@gmail.com>
2022-07-13 16:15:05 +0900

    [BKD] : 스택 연습문제 풀이

--- BKD/0x05/practice/6198.cpp
+++ BKD/0x05/practice/6198.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-  int N;
-  cin >> N;
+    int N;
+    cin >> N;
 
-  vector<int> h;
-  for (int i = 0; i < N; i++) {
-    int height;
-    cin >> height;
-    h.push_back(height);
-  }
+    vector<pair<int, int>> h;
+    for (int i = 1; i <= N; i++)
+    {
+        int height;
+        cin >> height;
+        h.push_back({height, i});
+    }
 
-  stack<int> tower({0});
-  for (int i = 0; i < N; i++) {
-    // 벡터의 뒤분터 순회
-    while (tower.top() > h.back()) tower.pop();
-    cout << tower.size() << ' ';
-    tower.push(h.back());
-    h.pop_back();
-  }
+    int ans = 0;
+    for (int i = 0; i < N; i++) // 벡터를 뒤에서부터 순회
+    {
+        stack<pair<int, int>> tower;
+        for (int j = i + 1; j < N; j++)
+        {
+            if (h[j].first < h[i].first)
+                tower.push(h[j]);
+            else
+                break;
+        }
+        // cout << tower.size() << '\n';
+        ans += tower.size();
+    }
+    cout << ans;
 }
