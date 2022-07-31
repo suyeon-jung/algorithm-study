73ed9494e9d217da92106746fd859269129d5190
suyeon-jung <wjdjsy210@gmail.com>
2022-07-31 15:20:49 +0900

    [BKD] : 백트래킹 연습문제 풀이
    - 1182(부분 수열의 합)

--- BKD/0x0C/부분수열의합.cpp
+++ BKD/0x0C/부분수열의합.cpp
@@ -4,18 +4,14 @@ using namespace std;
 int n, s;
 int arr[30];
 int cnt = 0;
-
 // 현재 cur 인덱스 수를 더할지 말지 정하기
 void func(int cur, int sum)
 {
   if (cur == n)
   {
     if (sum == s)
-    {
       cnt++;
-      cout << cnt;
-      return;
-    }
+    return;
   }
   func(cur + 1, sum);
   func(cur + 1, sum + arr[cur]);
@@ -29,7 +25,7 @@ int main()
   for (int i = 0; i < n; i++)
     cin >> arr[i];
   func(0, 0);
-  if (s == 0)
+  if (s == 0) // 아무것도 선택하지 않는 수열의 경우 제외
     cnt--;
   cout << cnt;
   return 0;
